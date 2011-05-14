88899f5ebbdc007f83ded2aabe53a3739181af19
mrsteveman1 <mrsteveman1@gmail.com>
2011-05-13 21:22:24 -0400

    update copyright, add readme, move ASI and json into the tree

--- BitTicker/Ticker.m
+++ BitTicker/Ticker.m
@@ -1,6 +1,21 @@
-//  Created by steve on 5/2/10.
-//  Copyright 2010 none. All rights reserved.
-//
+/*
+ BitTicker is Copyright 2011 Stephen Oliver
+ http://github.com/mrsteveman1
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+*/
 
 #import "Ticker.h"
 #import "ASIFormDataRequest.h"

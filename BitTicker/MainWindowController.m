d6bf438b03f663b4e6d2825ab69251e4c3b72775
mrsteveman1 <mrsteveman1@gmail.com>
2011-06-12 18:23:19 -0400

    default panel for main window, normal text colors and method for correctly showing a highlighted table row

--- BitTicker/MainWindowController.m
+++ BitTicker/MainWindowController.m
@@ -43,6 +43,8 @@
 }
 
 #pragma mark - Table view
+
+
 - (NSInteger)numberOfRowsInTableView:(NSTableView *)aTableView {
     return eNumberOfMarkets;
 }
@@ -51,6 +53,23 @@
 	return rowString;
 	
 }
+
+- (void)tableView:(NSTableView *)aTableView willDisplayCell:(id)aCell forTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex {
+	NSString *rowString = [sharedSettings stringForMarket:rowIndex];
+	NSColor *txtColor;
+	if ([self.marketListTable selectedRow] == rowIndex) {
+		txtColor = [NSColor whiteColor];
+	}
+	else {
+		txtColor = [NSColor blackColor];
+	}
+
+	NSFont *txtFont = [NSFont boldSystemFontOfSize:14];
+	NSDictionary *txtDict = [NSDictionary dictionaryWithObjectsAndKeys: txtFont, NSFontAttributeName, txtColor, NSForegroundColorAttributeName, nil];
+	NSAttributedString *attrStr = [[[NSAttributedString alloc] initWithString:rowString attributes:txtDict] autorelease];
+	[aCell setAttributedStringValue:attrStr];
+}
+
 - (void)tableViewSelectionDidChange:(NSNotification *)aNotification {
     selectedMarket = [self.marketListTable selectedRow];
 	NSString *string = [sharedSettings stringForMarket:selectedMarket];
